1f1e8f6ba7ac7e512f8069364bdb436a48ec6a3a
OtienoSamwel <otienosamwel135@gmail.com>
2021-06-21 17:02:04 +0300

    initial commit

---
+++ app/src/main/java/com/os/castsinapod/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.os.castsinapod.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.os.castsinapod.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
