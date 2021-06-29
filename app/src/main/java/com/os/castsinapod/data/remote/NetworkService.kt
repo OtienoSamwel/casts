bf7d3e8b58cbbb92cdc38ddfe9a817392490a9dd
OtienoSamwel <otienosamwel135@gmail.com>
2021-06-29 21:38:25 +0300

    - use hilt to provide retrofit dependencies as a singleton

--- app/src/main/java/com/os/castsinapod/data/remote/NetworkService.kt
+++ app/src/main/java/com/os/castsinapod/data/remote/NetworkService.kt
@@ -1,23 +1,32 @@
-package com.os.castsinapod.data.network
+package com.os.castsinapod.data.remote
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkService {
     private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
     private val client: OkHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
-    private const val baseUrl = "https://api.spotify.com/v1"
+    private const val baseUrl = "https://listen-api-test.listennotes.com/"
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
         .client(client)
         .baseUrl(baseUrl)
         .build()
 
-    val service: ApiEndpoints by lazy {
-        retrofit.create(ApiEndpoints::class.java)
+    @Provides
+    @Singleton
+    fun provideApi(): ApiEndpoints {
+        return retrofit.create(ApiEndpoints::class.java)
     }
 }
