5116113021c8aec489cc0264667ffa0af1500594
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 00:19:09 +0300

    - create constants to use with service

---
+++ app/src/main/java/com/os/castsinapod/ui/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.os.castsinapod.ui.utils
+
+object Constants {
+    const val NOTIFICATION_ID = 200
+    const val NOTIFICATION_CHANNEL_NAME = "notification channel 1"
+    const val NOTIFICATION_CHANNEL_ID = "notification channel id 1"
+}
