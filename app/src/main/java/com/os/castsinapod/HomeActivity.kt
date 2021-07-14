0f269256cd86166c605eb4035463b7abdd69ed06
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 00:22:09 +0300

    -  move viewmodel and recyclerview to HomeFragment.kt

--- app/src/main/java/com/os/castsinapod/HomeActivity.kt
+++ app/src/main/java/com/os/castsinapod/HomeActivity.kt
@@ -1,24 +1,20 @@
 package com.os.castsinapod
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.os.castsinapod.databinding.ActivityHomeBinding
-import com.os.castsinapod.ui.home.adapters.SearchAdapter
-import com.os.castsinapod.ui.home.viewmodels.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity() {
 
-    private val viewModel: HomeViewModel by viewModels()
     private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        recyclerView = binding.recyclerView
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
