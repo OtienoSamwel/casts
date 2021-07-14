afafa965914f5467bf03e7ea3f0fe73c0deadd4c
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 00:25:21 +0300

    -  create single instance of exoplayer media player object to use in service and drawer fragment

---
+++ app/src/main/java/com/os/castsinapod/ui/utils/Player.kt
@@ -0,0 +1,21 @@
+package com.os.castsinapod.ui.utils
+
+import android.content.Context
+import com.google.android.exoplayer2.SimpleExoPlayer
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Player {
+
+    @Provides
+    @Singleton
+    fun getPlayer(@ApplicationContext context: Context): SimpleExoPlayer {
+        return SimpleExoPlayer.Builder(context).build()
+    }
+}
