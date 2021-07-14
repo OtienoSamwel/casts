838308fbf96747fca8e5bb226dba50cd672b1e5a
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 00:23:13 +0300

    -  create a home fragment for the app

---
+++ app/src/main/java/com/os/castsinapod/ui/home/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.os.castsinapod.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.os.castsinapod.R
+import com.os.castsinapod.ui.home.adapters.SearchAdapter
+import com.os.castsinapod.ui.home.viewmodels.HomeFragmentViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private val viewModel: HomeFragmentViewModel by viewModels()
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.home_fragment_recycler_view)
+        viewModel.getResults("harry")
+        viewModel.data.observe(viewLifecycleOwner) {
+            recyclerView.adapter = SearchAdapter(it, requireContext()) { onClick() }
+        }
+    }
+
+    private fun onClick(){
+        val action = HomeFragmentDirections.actionHomeFragmentToMediaDrawerFragment()
+        view?.findNavController()?.navigate(action)
+    }
+}
