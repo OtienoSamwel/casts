e9513492cd95620667580f7ff8087b06082d472c
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-15 00:26:15 +0300

    -  create adapter for exoplayer notification

---
+++ app/src/main/java/com/os/castsinapod/ui/services/adapter/PlayerDescriptionAdapter.kt
@@ -0,0 +1,27 @@
+package com.os.castsinapod.ui.services.adapter
+
+import android.app.PendingIntent
+import android.graphics.Bitmap
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.PlayerNotificationManager
+
+class PlayerDescriptionAdapter(private val intent: PendingIntent): PlayerNotificationManager.MediaDescriptionAdapter {
+    override fun getCurrentContentTitle(player: Player): CharSequence {
+        return player.currentMediaItem.toString()
+    }
+
+    override fun createCurrentContentIntent(player: Player): PendingIntent? {
+        return intent
+    }
+
+    override fun getCurrentContentText(player: Player): CharSequence? {
+        return "This is just a test"
+    }
+
+    override fun getCurrentLargeIcon(
+        player: Player,
+        callback: PlayerNotificationManager.BitmapCallback
+    ): Bitmap? {
+        return null
+    }
+}
