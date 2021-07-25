1d1fe10105ff3449c8ee62bea741b4826661cc59
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:24:06 +0300

    - hilt annotation

--- app/src/main/java/com/os/castsinapod/ui/details/DetailFragment.kt
+++ app/src/main/java/com/os/castsinapod/ui/details/DetailFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.os.castsinapod.R
 
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     override fun onCreateView(
