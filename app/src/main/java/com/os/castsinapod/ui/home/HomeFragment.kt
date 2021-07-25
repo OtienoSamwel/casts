85abce24fa35b42d9749cbd246f98ccd46af037c
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:30:43 +0300

    - switch to viewBinding

--- app/src/main/java/com/os/castsinapod/ui/home/HomeFragment.kt
+++ app/src/main/java/com/os/castsinapod/ui/home/HomeFragment.kt
@@ -22,11 +22,10 @@ class HomeFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
