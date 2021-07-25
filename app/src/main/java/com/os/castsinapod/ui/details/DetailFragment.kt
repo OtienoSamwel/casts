6bedb46bb78c50d11bdc41e80bd76d3cf3b91651
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:26:26 +0300

    - inflate and set the views

--- app/src/main/java/com/os/castsinapod/ui/details/DetailFragment.kt
+++ app/src/main/java/com/os/castsinapod/ui/details/DetailFragment.kt
@@ -1,11 +1,19 @@
 package com.os.castsinapod.ui.details
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.os.castsinapod.R
+import com.os.castsinapod.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
@@ -18,9 +26,33 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        _binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getPodcast(args.podcastId)
+
+        viewModel.data.observe(viewLifecycleOwner) {
+            with(binding) {
+                podcastTitle.text = it.title
+                podcastAuthor.text = it.publisher
+                descriptionTxt.text = it.description
+                numberOfEpisodesTxt.text =
+                    getString(R.string.episodes, it.total_episodes, "Episodes")
+
+                Glide.with(this@DetailFragment)
+                    .load(it.image)
+                    .placeholder(ColorDrawable(Color.GRAY))
+                    .error(ColorDrawable(Color.RED))
+                    .centerCrop()
+                    .transform(RoundedCorners(10))
+                    .into(podcastImage)
+            }
+        }
+
     }
 
     override fun onDestroy() {
