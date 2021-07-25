10e3cdbc04fda8dcae57a6a46071522dcffeea67
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:32:56 +0300

    - import required instance

--- app/src/main/java/com/os/castsinapod/ui/home/viewmodels/HomeFragmentViewModel.kt
+++ app/src/main/java/com/os/castsinapod/ui/home/viewmodels/HomeFragmentViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.os.castsinapod.domain.models.PodcastResponse
+import com.os.castsinapod.domain.models.PodcastsResponse
 import com.os.castsinapod.domain.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,8 +13,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeFragmentViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
-    private val  _data = MutableLiveData<PodcastResponse>()
-    var data : LiveData<PodcastResponse> = _data
+    private val  _data = MutableLiveData<PodcastsResponse>()
+    val data : LiveData<PodcastsResponse> = _data
 
 
     fun getResults(q: String) {
