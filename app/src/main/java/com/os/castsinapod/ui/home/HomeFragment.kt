cc98b4d612c7408cc4c673f69f61717623eed342
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:32:08 +0300

    - remove unused import

--- app/src/main/java/com/os/castsinapod/ui/home/HomeFragment.kt
+++ app/src/main/java/com/os/castsinapod/ui/home/HomeFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.os.castsinapod.databinding.FragmentHomeBinding
