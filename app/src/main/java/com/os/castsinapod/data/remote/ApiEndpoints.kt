b3b83c6d44be1b6605477db04b1fa25c7bb164ba
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-25 14:23:24 +0300

    - create a get request with path parameter for a single podcast's details

--- app/src/main/java/com/os/castsinapod/data/remote/ApiEndpoints.kt
+++ app/src/main/java/com/os/castsinapod/data/remote/ApiEndpoints.kt
@@ -1,11 +1,15 @@
 package com.os.castsinapod.data.remote
 
 import com.os.castsinapod.domain.models.PodcastResponse
-import kotlinx.coroutines.flow.Flow
+import com.os.castsinapod.domain.models.PodcastsResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiEndpoints {
     @GET("/api/v2/search")
-    suspend fun searchPodcasts(@Query("q") q: String): PodcastResponse
+    suspend fun searchPodcasts(@Query("q") q: String): PodcastsResponse
+
+    @GET("api/v2/podcasts/{id}")
+    suspend fun getPodcast(@Path("id") id: String) : PodcastResponse
 }
