8b91b823c5c81b79b00119654331732ac9bf4e13
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-27 23:06:55 +0300

    - switch to view binding and add adapter for recycler view

--- app/src/main/java/com/os/castsinapod/ui/explore/features/ExploreFeaturesFragment.kt
+++ app/src/main/java/com/os/castsinapod/ui/explore/features/ExploreFeaturesFragment.kt
@@ -4,26 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.os.castsinapod.R
+import androidx.fragment.app.viewModels
+import com.os.castsinapod.databinding.FragmentExploreFeaturesBinding
 import com.os.castsinapod.ui.explore.adapters.ExploreFragmentAdapter
+import com.os.castsinapod.ui.home.adapters.SearchAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ExploreFeaturesFragment : Fragment() {
+    private var _binding: FragmentExploreFeaturesBinding? = null
+    private val binding: FragmentExploreFeaturesBinding get() = _binding!!
+    private val viewModel: ExploreFeaturesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore_features, container, false)
+        _binding = FragmentExploreFeaturesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val text: TextView = view.findViewById(R.id.exp_feature_text)
+        viewModel.getResults(ExploreFragmentAdapter.myFragmentLabels[ExploreFragmentAdapter.positioning])
+        viewModel.data.observe(viewLifecycleOwner) {
+            binding.exploreFeaturesRecyclerview.adapter = SearchAdapter(it)
+        }
+    }
 
-        text.text = ExploreFragmentAdapter.myFragmentLabels[ExploreFragmentAdapter.positioning]
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
