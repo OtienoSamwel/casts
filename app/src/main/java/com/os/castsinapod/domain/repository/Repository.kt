989cf70d1a260e6da8982fb0540cf0d3775cb5a0
OtienoSamwel <otienosamwel135@gmail.com>
2021-07-27 23:13:37 +0300

    - add method to retrieve a single podcast

--- app/src/main/java/com/os/castsinapod/domain/repository/Repository.kt
+++ app/src/main/java/com/os/castsinapod/domain/repository/Repository.kt
@@ -1,6 +1,7 @@
 package com.os.castsinapod.domain.repository
 
 import com.os.castsinapod.domain.models.PodcastResponse
+import com.os.castsinapod.domain.models.PodcastsResponse
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -8,7 +9,8 @@ import dagger.hilt.android.components.ViewModelComponent
 
 
 interface Repository {
-    suspend fun getPodcasts(q: String): PodcastResponse
+    suspend fun getPodcasts(q: String): PodcastsResponse
+    suspend fun getPodcast(id: String): PodcastResponse
 }
 
 @Module
