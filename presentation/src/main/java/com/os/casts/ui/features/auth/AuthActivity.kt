8ba414c33f6ee0946c357509128f9ef43a97f8d7
OtienoSamwel <otienosamwel135@gmail.com>
2022-03-26 20:44:57 +0300

    create auth activity

---
+++ presentation/src/main/java/com/os/casts/ui/features/auth/AuthActivity.kt
@@ -0,0 +1,31 @@
+package com.os.casts.ui.features.auth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.os.casts.ui.theme.CastTheme
+
+class AuthActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CastTheme {
+                Surface(
+                    modifier = Modifier.fillMaxWidth(),
+                    color = MaterialTheme.colors.background
+                ) { Content() }
+            }
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    Text(text = "Auth", style = MaterialTheme.typography.h1)
+}
